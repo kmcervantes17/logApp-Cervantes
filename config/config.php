5558b3d3f9488702f00668f90bc8f609f891b8f1
Kiefer Cervantes <90808927+kmcervantes17@users.noreply.github.com>
2021-10-28 18:58:25 +0800

    DONE

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/REDEMPLE/logApp-scaling-octo');
+	define('DB_HOST', 'http://sql6.freesqldatabase.com/');
+	define('DB_USER', 'sql6447363');
+	define('DB_PASS', 'H3gsmzD6Ir');
+	define('DB_NAME', 'sql6447363');
