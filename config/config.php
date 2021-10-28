19bfdee3cc014c28feb9f80431d842b4727e5965
Kiefer Cervantes <90808927+kmcervantes17@users.noreply.github.com>
2021-10-28 21:09:47 +0800

    done

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
-	define('ROOT_URL', 'https://safe-sands-48808.herokuapp.com/');
-	define('DB_HOST', 'http://sql6.freesqldatabase.com/');
-	define('DB_USER', 'sql6447363');
-	define('DB_PASS', 'H3gsmzD6Ir');
-	define('DB_NAME', 'sql6447363');
+	define('ROOT_URL', 'https://safe-sands-48808.herokuapp.com');
+	define('DB_HOST', 'http:/sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6447377');
+	define('DB_PASS', 'tzCCPaDxDd');
+	define('DB_NAME', 'sql6447377');
+
+	?>
